a7b49bc54823f5eebf1c99f946ca0217280abd8c
Chih-Kai Tseng <chih-kai-tseng@Chih-KaideMacBook-Pro.local>
2021-10-16 10:11:59 +0800

    First commit for init spring boot project

---
+++ spring-boot/backend/src/main/java/com/example/backend/service/ResponseService.java
@@ -0,0 +1,10 @@
+package com.example.backend.service;
+
+import com.example.backend.model.ResponseFormat;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public interface ResponseService {
+    public ResponseEntity<ResponseFormat> basic(HttpStatus httpStatus, String msg, Object data);
+}
